7afba55648e6d6da42debfe2fe78402c76acd224
AleSandi22 <alesf22@gmail.com>
2022-05-03 12:04:21 -0600

    initial

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true,
+		'node': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended'
+	],
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		'ecmaFeatures': {
+			'tsx': true
+		},
+		'ecmaVersion': 'latest',
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'react',
+		'@typescript-eslint'
+	],
+	'rules': {
+		'react/react-in-jsx-scope': 'off',
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		]
+	},
+	'settings': {
+		'react': {
+			'version': 'detect'
+		}
+	}
+};
