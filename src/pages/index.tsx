7f546295c51c899fe7aa732c4363b15247032231
AleSandi22 <alesf22@gmail.com>
2022-05-03 18:41:18 -0600

    nav layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,11 @@
+import Nav from '../components/nav';
 import styles from '../styles/Home.module.scss';
 
 export default function Home() {
-  return <div className={styles.home}></div>;
+  return (
+    <div className={styles.home}>
+      <Nav />
+      <main></main>
+    </div>
+  );
 }
