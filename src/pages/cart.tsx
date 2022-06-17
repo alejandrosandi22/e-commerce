c5a7239f9c181c125a53bc49f7bd564507f7cc90
alejandrosandi22 <alesf22@gmail.com>
2022-06-17 16:00:53 -0600

    resolve conflicts

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -2,6 +2,7 @@ import useFetch from 'hooks/useFetch';
 import { useEffect } from 'react';
 
 export default function Cart() {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const { data, loading } = useFetch<any>(
     '/api/cart?id=62acc1baea78d4d028a69e5b'
   );
